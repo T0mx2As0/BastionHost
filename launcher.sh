f0b1c0b76760191b8ee837977da56edc4a9d4048
tommaso trivelli <tommasotrivelli@Mac.homenet.telecomitalia.it>
2025-05-30 16:20:52 +0200

    update launcher

--- launcher.sh
+++ launcher.sh
@@ -8,7 +8,7 @@ echo "cerco se ci sono Bastian Host già esistenti.... "
 
 n_instances=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=BastionHost" "Name=instance-state-name,Values=running" --output text | wc -l)
 
-if ["n_instances" -g 0 ]; then
+if ["$n_instances" -g 0 ]; then
     
     echo "esiste gia un istanza Bastian Host ...."
 
@@ -20,10 +20,12 @@ else
     terraform init 
 
     echo "esecuzione Terraform plan "    
-    terraform plan > ./log/log-plan.txt 2>&1 &
+    terraform plan > ./log/log-plan.txt 2>&1
 
     echo "esecuzione Terraform apply "
-    terraform apply ./log/log-apply.txt 2>&1 &
+    terraform apply -auto-approve > ./log/log-apply.txt 2>&1
 
 
     echo "esecuzione riuscita ....."
+
+fi
